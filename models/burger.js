a3e7d2712e019a69cd0d876b3e8b507c893cb9d4
Jake Thornberry <wjthornberry@gmail.com>
2017-08-22 21:13:39 -0500

    Initial commit: models complete

---
+++ models/burger.js
@@ -0,0 +1,27 @@
+// Imports the ORM to create functions that will interact with the database
+var orm = require('../config/orm.js');
+
+var burger = {
+    all: function(cb) {
+        orm.selectAll('burgers', function(res) {
+            cb(res);
+        });
+    },
+    create: function(cb) {
+        orm.insertOne('burgers', vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(cb) {
+        orm.updateOne('burgers', objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    delete: function(cb) {
+        orm.delete('burgers', condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
